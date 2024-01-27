c291604cf1465cd0906e552b6454d35cf8c89d61
HANFU XIAO <hxiao15@mySeneca.ca>
2024-01-27 14:45:16 -0500

    finished workshop1

--- WS01/DIY/StMark.h
+++ WS01/DIY/StMark.h
@@ -9,6 +9,18 @@ Revision History
 -----------------------------------------------------------
 Name            Date            Reason
 ***********************************************************************/
+/*/////////////////////////////////////////////////////////////////////////
+                        Workshop1 Part2
+Full Name  : Hanfu Xiao
+Student ID#: 033503145
+Email      : hxiao15@myseneca.ca
+Section    : NFF
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
 #ifndef SENECA_STMARK_H
 #define SENECA_STMARK_H
 namespace seneca {
@@ -22,6 +34,9 @@ namespace seneca {
       int mark;
    };
 
+   bool printReport(const char* filename);
+   void bubbleSort(StMark* marks, int size);
+   void printSortedMarks(const StMark* marks, int size);
 }
 #endif // !SENECA_STMARK_H
 
